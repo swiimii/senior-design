fa54c67165625127cba7dc7dcb45837fbe3c75a6
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-11-19 18:09:58 -0500

    -input changes and player prefab

--- Senior Design/Assets/Scripts/Input/InputReader.cs
+++ Senior Design/Assets/Scripts/Input/InputReader.cs
@@ -2,9 +2,18 @@
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
 
+public struct InputState {
+    public Vector2 movementDirection;
+}
+
+public interface IInputProvider {
+    public InputState GetState();
+    void EnableInput();
+    void DisableInput();
+}
+
 [CreateAssetMenu(fileName = "InputReader", menuName = "InputData/Input Reader")]
-public class InputReader : ScriptableObject, GameInput.IGameplayActions {
-    
+public class InputReader : ScriptableObject, IInputProvider, GameInput.IGameplayActions {
     // Gameplay
     public event UnityAction<Vector2> MoveEvent;
     public event UnityAction<Vector2> MousePosEvent;
@@ -14,41 +23,52 @@ public class InputReader : ScriptableObject, GameInput.IGameplayActions {
 
     private GameInput GameInput { get; set; }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         if (GameInput == null) {
             GameInput = new GameInput();
             GameInput.Gameplay.SetCallbacks(this);
         }
-
-        EnableGameplayInput();
+        EnableInput();
     }
 
-    private void OnDisable() => DisableAllInput();
+    private void OnDisable() => DisableInput();
 
     #region Gameplay Actions
-    
-    public void OnMove(InputAction.CallbackContext context) {
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
         MoveEvent?.Invoke(context.ReadValue<Vector2>().normalized);
     }
 
-    public void OnInteract(InputAction.CallbackContext context) {
+    public void OnInteract(InputAction.CallbackContext context)
+    {
         if (InteractionStartedEvent != null && context.phase == InputActionPhase.Started)
             InteractionStartedEvent.Invoke();
 
         if (InteractionCancelledEvent != null && context.phase == InputActionPhase.Canceled)
             InteractionCancelledEvent.Invoke();
     }
-    public void OnMouse(InputAction.CallbackContext context) {
+
+    public void OnMouse(InputAction.CallbackContext context)
+    {
         MousePosEvent?.Invoke(context.ReadValue<Vector2>());
     }
 
     #endregion
 
-    public void EnableGameplayInput() {
+    public InputState GetState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EnableInput()
+    {
         GameInput.Gameplay.Enable();
     }
-    
-    public void DisableAllInput() {
+
+    public void DisableInput()
+    {
         GameInput.Gameplay.Disable();
     }
 }
