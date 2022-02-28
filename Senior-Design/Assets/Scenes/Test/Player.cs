3f19d5350f7eca11baf8c9aeae92f975375dbe70
vazeraja <68131084+vazeraja@users.noreply.github.com>
2022-02-28 15:57:50 -0500

    Change to FixedUpdate

--- Senior-Design/Assets/Scenes/Test/Player.cs
+++ Senior-Design/Assets/Scenes/Test/Player.cs
@@ -49,7 +49,7 @@ public class Player : NetworkBehaviour {
         }
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         SetOverlay();
         
         if (ClientAndOwner) {
