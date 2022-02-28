916f759b22373caf8d74828bd319c885aae01eb0
vazeraja <68131084+vazeraja@users.noreply.github.com>
2022-02-28 15:55:36 -0500

    some code clean up and file reorg

--- Senior-Design/Assets/Scenes/Test/Player.cs
+++ Senior-Design/Assets/Scenes/Test/Player.cs
@@ -21,6 +21,7 @@ public class Player : NetworkBehaviour {
         public const string PlayerWest = "PlayerWest";
     }
     private static InputState inputState => InputManager.Instance.InputProvider;
+    private bool ClientAndOwner => IsClient && IsOwner;
 
     private Rigidbody2D rb;
     private CollisionDetection cd;
@@ -48,57 +49,59 @@ public class Player : NetworkBehaviour {
         }
     }
 
-    private void Start() {
-        if (IsClient && IsOwner) {
-            transform.position = new Vector3(0, 0, 0);
-        }
-    }
-
     private void Update() {
         SetOverlay();
         
-        if (IsClient && IsOwner) {
-            ClientMovementAndAnimation();
+        if (ClientAndOwner) {
+            UpdateClientMovementAndAnimation();
             cd.ActivateTopDownCollision(inputState.movementDirection);
         }
         
-        ClientVisuals();
+        UpdateClientAnimationSpeed();
     }
 
-    private void ClientVisuals() {
+    private void UpdateClientAnimationSpeed() {
         anim.speed = networkPlayerState.Value == PlayerState.Idle ? 0 : 1;
     }
 
-    private void ClientMovementAndAnimation() {
+    private void UpdateClientMovementAndAnimation() {
         rb.velocity = inputState.movementDirection * walkSpeed;
 
         if (inputState.movementDirection.y > 0) {
             if (inputState.movementDirection.x > 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerNorthEast);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerNorthEast);
             }
             else if (inputState.movementDirection.x < 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerNorthWest);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerNorthWest);
             }
             else {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerNorth);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerNorth);
             }
         }
         else if (inputState.movementDirection.y < 0) {
             if (inputState.movementDirection.x > 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerSouthEast);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerSouthEast);
             }
             else if (inputState.movementDirection.x < 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerSouthWest);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerSouthWest);
             }
             else {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerSouth);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerSouth);
             }
         }
         else {
             if (inputState.movementDirection.x > 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerEast);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerEast);
             }
             else if (inputState.movementDirection.x < 0) {
+                UpdateAnimationClipNameServerRpc(AnimationStates.PlayerWest);
                 PlayAnimationClipServerRpc(AnimationStates.PlayerWest);
             }
             else {
@@ -107,7 +110,7 @@ public class Player : NetworkBehaviour {
         }
     }
 
-    public void SetOverlay() {
+    private void SetOverlay() {
         if (overlaySet || string.IsNullOrEmpty(playerNetworkName.Value)) return;
         var localPlayerOverlay = gameObject.GetComponentInChildren<TMP_Text>();
         localPlayerOverlay.text = $"{playerNetworkName.Value}";
@@ -115,18 +118,16 @@ public class Player : NetworkBehaviour {
     }
 
     [ServerRpc]
-    private void PauseAnimationsServerRpc() {
-        UpdatePlayerStateServerRpc(PlayerState.Idle);
-    }
+    private void UpdateAnimationClipNameServerRpc(string clipName) => animationClipName.Value = clipName;
+
+    [ServerRpc]
+    private void PauseAnimationsServerRpc() => UpdatePlayerStateServerRpc(PlayerState.Idle);
 
     [ServerRpc]
     private void PlayAnimationClipServerRpc(string clipName) {
         anim.Play(clipName);
         UpdatePlayerStateServerRpc(PlayerState.Walk);
     }
-
     [ServerRpc]
-    private void UpdatePlayerStateServerRpc(PlayerState state) {
-        networkPlayerState.Value = state;
-    }
+    private void UpdatePlayerStateServerRpc(PlayerState state) => networkPlayerState.Value = state;
 }
