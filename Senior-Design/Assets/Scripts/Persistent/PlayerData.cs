97c01cff2ef2d1e5dd001506b288fc9ec2ca3b02
vazeraja <68131084+vazeraja@users.noreply.github.com>
2022-02-28 19:15:07 -0500

    Update PlayerData.cs

--- Senior-Design/Assets/Scripts/Persistent/PlayerData.cs
+++ Senior-Design/Assets/Scripts/Persistent/PlayerData.cs
@@ -21,9 +21,4 @@ public struct PlayerData : INetworkSerializable, System.IEquatable<PlayerData> {
         serializer.SerializeValue(ref Name);
         serializer.SerializeValue(ref ClientId);
     }
-
-    public static implicit operator string(PlayerData p) => p.ToString();
-    public static implicit operator PlayerData(string s) => new PlayerData {
-        Name = new FixedString32Bytes(s)
-    };
 }
